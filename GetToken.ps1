5ef31aa21fbd5653aae2d6cc1fcc8faaf0f5709d
Collin Bardini <cbardini@osisoft.com>
2022-01-05 14:54:55 -0800

    Update GetToken.ps1

--- GetToken.ps1
+++ GetToken.ps1
@@ -8,7 +8,7 @@ $ClientSecret = $Appsettings.ClientSecret
 
 
 # Step 2: get the authentication endpoint from the discovery URL
-$DiscoveryUrlRequest = Invoke-WebRequest -Uri ($Resource + "/identity/.well-known/openid-configuration") -Method Get
+$DiscoveryUrlRequest = Invoke-WebRequest -Uri ($Resource + "/identity/.well-known/openid-configuration") -Method Get -UseBasicParsing
 $DiscoveryBody = $DiscoveryUrlRequest.Content | ConvertFrom-Json
 $TokenUrl = $DiscoveryBody.token_endpoint
 
@@ -19,13 +19,13 @@ $TokenForm = @{
     grant_type = "client_credentials"
 }
 
-$TokenRequest = Invoke-WebRequest -Uri $TokenUrl -Body $TokenForm -Method Post -ContentType "application/x-www-form-urlencoded"
+$TokenRequest = Invoke-WebRequest -Uri $TokenUrl -Body $TokenForm -Method Post -ContentType "application/x-www-form-urlencoded" -UseBasicParsing
 $TokenBody = $TokenRequest | ConvertFrom-Json
 
 # Step 4: test token by calling the base tenant endpoint
 $AuthHeader = @{
     Authorization = "Bearer " + $TokenBody.access_token
 }
-$TenantRequest = Invoke-WebRequest -Uri ($Resource + "/api/" + $ApiVersion + "/Tenants/" + $TenantId) -Method Get -Headers $AuthHeader
+$TenantRequest = Invoke-WebRequest -Uri ($Resource + "/api/" + $ApiVersion + "/Tenants/" + $TenantId) -Method Get -Headers $AuthHeader -UseBasicParsing
 
 $TenantRequest
